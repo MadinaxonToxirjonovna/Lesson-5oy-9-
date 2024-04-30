755d4047b49cb92683ada759167e9def43a4227e
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-04-30 19:07:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.sumbit');
+id = document.querySelector('.id');
+parol = document.querySelector('.password')
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+
+    
+    if (id.value == "123456" && parol.value == "78900") {
+        alert("Siz spacega muvaffaqiyatli kirdingiz")
+    } else {
+        alert("ID yoki parol xato")
+    }
+
+
+    id.value = ""
+    parol.value = ""
+})
